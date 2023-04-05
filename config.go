84697790df331963064951e944449d5844235121
Altair Inglorious <>
2023-04-05 14:18:03 +0300

    new action on init

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package bot
 
 type BotConfig struct {
+	onInit      func(*Bot)
 	onChat      func(*Bot, []byte)
 	onWhisper   func(*Bot, []byte)
 	onUserJoin  func(*Bot, []byte)
@@ -13,37 +14,26 @@ func NewBotConfig() *BotConfig {
 		onWhisper:   func(*Bot, []byte) {},
 		onUserJoin:  func(*Bot, []byte) {},
 		onUserLeave: func(*Bot, []byte) {},
+		onInit:      func(*Bot) {},
 	}
-}
-
-func (b *Bot) OnChat(handler func(*Bot, []byte)) {
-	b.config.onChat = handler
 }
 
 func (c *BotConfig) OnChat(handler func(*Bot, []byte)) {
 	c.onChat = handler
 }
 
-func (b *Bot) OnWhisper(handler func(*Bot, []byte)) {
-	b.config.onWhisper = handler
-}
-
 func (c *BotConfig) OnWhisper(handler func(*Bot, []byte)) {
 	c.onWhisper = handler
-}
-
-func (b *Bot) OnUserJoin(handler func(*Bot, []byte)) {
-	b.config.onUserJoin = handler
 }
 
 func (c *BotConfig) OnUserJoin(handler func(*Bot, []byte)) {
 	c.onUserJoin = handler
 }
 
-func (b *Bot) OnUserLeave(handler func(*Bot, []byte)) {
-	b.config.onUserLeave = handler
-}
-
 func (c *BotConfig) OnUserLeave(handler func(*Bot, []byte)) {
 	c.onUserLeave = handler
+}
+
+func (c *BotConfig) OnInit(handler func(*Bot)) {
+	c.onInit = handler
 }
